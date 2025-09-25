c8f66c24ac4d0d8bb177211c2cca2a231d94b8b3
nimnusmik <kimsunmin@gmail.com>
2025-09-26 08:46:33 +0900

    add visualize and rerun.py

--- src/main.py
+++ src/main.py
@@ -67,8 +67,8 @@ kfold_results = train_dcn_kfold(
     lr= CFG['LEARNING_RATE'],
 
     device=device,
-    alpha=0.7,  #0.7->0.6->0.7
-    margin=1.0, #1.0->1.5->1.0
+    alpha=0.5,  #0.7->0.6->0.7->0.5
+    margin=1.0, #1.0->1.5->1.0->1.5
     random_state=42,
 
     checkpoint_dir=CFG['CHECKPOINT_DIR'],
@@ -221,7 +221,7 @@ else:
 
 # 5. 제출 파일 생성
 os.makedirs(CFG['OUTPUT_PATH'], exist_ok=True)
-submission_filename = "dcn_submission0924.csv"
+submission_filename = "dcn_submission0925.csv"
 submission_path = os.path.join(CFG['OUTPUT_PATH'], submission_filename)
 submission = create_submission(
     test_preds=ensemble_preds,
